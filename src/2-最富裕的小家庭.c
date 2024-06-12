5efafd9153f4014c59aad0867e22fa400c3d40b3
huangjx-beijing <191611711@qq.com>
2024-06-12 10:40:15 +0800

    fix: bug

--- src/2-最富裕的小家庭.c
+++ src/2-最富裕的小家庭.c
@@ -20,6 +20,8 @@ int main()
     for (int i = 0; i < n; ++i)
     {
         scanf("%d", &(persons[i].value));
+        persons[i].child1 = NULL;
+        persons[i].child2 = NULL;
     }
     
     // 创建关系
